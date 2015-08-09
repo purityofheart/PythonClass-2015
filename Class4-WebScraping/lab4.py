199827a57d9933eeb083423463dec17ccb6a5524
betuld <betuldemirkaya@Betuls-MacBook-Pro.local>
2015-08-09 15:08:50 -0500

    class 4

---
+++ Class4-WebScraping/lab4.py
@@ -0,0 +1,8 @@
+#Go to https://polisci.wustl.edu/faculty/specialization
+#Go to the page for each of the professors.
+#Create a .csv file with the following information for each professor:
+	-Specialization
+	-Name
+	-Title
+	-E-mail
+	-Web page
