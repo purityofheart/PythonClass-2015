e9e9be39d6cb1758d0cb7087fd6ec49658edfa53
betuld <betuldemirkaya@betuls-mbp.dhcp.wustl.edu>
2015-08-14 14:28:55 -0500

    class 6

--- Class6-SortSearch/lab6_dc.py
+++ Class6-SortSearch/lab6_dc.py
@@ -16,9 +16,12 @@ def prime(number=121,list_of_primes=range(2,122),iters=0):
 
 def hanoi(n=3, first=([3,2,1],"first"), second=([], "second"), third=([], "third")):
     if n > 0:
+        print "I will now execute the first line with n: " + str(n) + "\n"
         hanoi(n - 1, first, third, second)
+        print "I will now execute the if statement with n: " + str(n) + "\n"
         if first[0]:
             disk = first[0].pop()
             print "move " + str(disk) + " from " + first[1] + " to " + third[1]
             third[0].append(disk)
+        print "I will now execute the final move with n: " + str(n) + "\n"
         hanoi(n - 1, second, first, third)
